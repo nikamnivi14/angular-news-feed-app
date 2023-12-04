78cb6422b541ea7a8892efa8e09aed215e4bc3b9
nikamnivi14 <147632443+nikamnivi14@users.noreply.github.com>
2023-12-05 05:00:08 +0530

    News Feed App

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewsListComponent } from './news-list/news-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'',component:NewsListComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ import { HttpClientModule } from '@angular/common/http';
+import { NewsFeedApiService } from './news-feed-api.service';
+import { NewsListComponent } from './news-list/news-list.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+ 
+ 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsListComponent,
+   
   ],
   imports: [
     BrowserModule, 
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    NgxPaginationModule
+    
+     
   ],
-  providers: [],
+  providers: [NewsFeedApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
